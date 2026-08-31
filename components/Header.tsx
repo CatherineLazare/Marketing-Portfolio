d09ddcb92f415e937332adbb097d6560ee7b1f6a
CatherineLazare <ms.battle@gmail.com>
2026-08-30 22:52:24 -0400

    Add components/Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,3 @@
+import Link from "next/link";
+export function Header(){return <header className="header"><div className="shell nav"><Link href="/" className="brand"><span>CL</span><b>Catherine Lazare</b></Link><nav aria-label="Primary"><Link href="/#work">Case Studies</Link><Link href="/#capabilities">Capabilities</Link><Link href="/#about">About</Link><Link href="/#resume">Resume</Link><Link href="/#contact">Contact</Link></nav><Link className="navCta" href="/#work">Explore work</Link></div></header>}
+
