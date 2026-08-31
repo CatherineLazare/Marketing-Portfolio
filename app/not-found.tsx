ae3f8edb285b96a58c4c69330a12a918e09dc0fc
CatherineLazare <ms.battle@gmail.com>
2026-08-30 22:52:35 -0400

    Add app/not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,2 @@
+import Link from "next/link";export default function NotFound(){return <main className="notFound"><h1>Case study not found.</h1><Link href="/">Return to the portfolio</Link></main>}
+
